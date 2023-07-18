afb672e86634070cf57bdf39edfd4e82a07d7dce
Albert Dulout <adulout@palo-it.com>
2023-07-18 16:34:11 +0800

    v0.0.1

---
+++ src/octokit.ts
@@ -0,0 +1,38 @@
+import {getInput, warning} from '@actions/core'
+import {Octokit} from '@octokit/action'
+import {retry} from '@octokit/plugin-retry'
+import {throttling} from '@octokit/plugin-throttling'
+
+const token = getInput('token') || process.env.GITHUB_TOKEN
+
+const RetryAndThrottlingOctokit = Octokit.plugin(throttling, retry)
+
+export const octokit = new RetryAndThrottlingOctokit({
+  auth: `token ${token}`,
+  throttle: {
+    onRateLimit: (
+      retryAfter: number,
+      options: any,
+      _o: any,
+      retryCount: number
+    ) => {
+      warning(
+        `Request quota exhausted for request ${options.method} ${options.url}
+Retry after: ${retryAfter} seconds
+Retry count: ${retryCount}
+`
+      )
+      return true
+    },
+    onSecondaryRateLimit: (_retryAfter: number, options: any) => {
+      warning(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`
+      )
+      return true
+    }
+  },
+  retry: {
+    doNotRetry: ['429'],
+    maxRetries: 10
+  }
+})
